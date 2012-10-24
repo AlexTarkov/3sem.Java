d782a835951374cb75f48fa1f92db331fbe13da2
Alex Tarkov <alex.tarkov@outlook.com>
2012-10-24 23:26:55 +0400

    Калькулятор закончен. Было бы не плохо вынести код по разбору строки в отдельный метод, и уже методом генерировать исключения, но для начала сойдет.

--- 8.lab/Calculator/src/calculator/Calculator.java
+++ 8.lab/Calculator/src/calculator/Calculator.java
@@ -16,24 +16,42 @@ public class Calculator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String input = JOptionPane.showInputDialog("Введите число N");
+        String input = JOptionPane.showInputDialog("Введите выражение");
         String[] lexems = input.split("[ ]");
         Queue q = new Queue();
         int mult = 1;
-        for (String str : lexems) {
-            if ((str.equals("+") || (str.equals("-"))) && (mult == 0)) {
-                if (str.equals("+")) {
-                    mult = 1;
+        String toException = "";
+        try {
+            for (String str : lexems) {
+                toException = str;
+                if ((str.equals("+") || (str.equals("-"))) && (mult == 0)) {
+                    if (str.equals("+")) {
+                        mult = 1;
+                    } else {
+                        mult = -1;
+                    }
                 } else {
-                    mult = -1;
-                }
-            } else {
-                int k = Integer.parseInt(str);
-                if ((mult != 0) && (k >= 0)) {
-                    q.add(mult * Integer.parseInt(str));
+                    int k = Integer.parseInt(str);
+                    if ((mult != 0) && (k >= 0) && (str.charAt(0) != '+')) {
+                        q.add(mult * Integer.parseInt(str));
+                        mult = 0;
+                    } else {
+                        throw new Exception();
+                    }
                 }
             }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, input + "= '" + toException + "' " 
+                    + ((mult == 0) ? "- Ожидался оператор +/-" : "- Ожидалось число"));
+            return;
+            //System.out.println(toException);
         }
+        
+        int result = 0;
+        while (q.hasNext()) {
+            result += q.getNext();
+        }
+        JOptionPane.showMessageDialog(null, input + " = " + result); 
     }
 }
 
