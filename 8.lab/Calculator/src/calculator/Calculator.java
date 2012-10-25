0c84f90ca86d0a9e63b0bd248dcddc77fecfadba
Alex Tarkov <alex.tarkov@outlook.com>
2012-10-25 11:02:55 +0400

    Калькулятор без очереди.

--- 8.lab/Calculator/src/calculator/Calculator.java
+++ 8.lab/Calculator/src/calculator/Calculator.java
@@ -18,9 +18,10 @@ public class Calculator {
     public static void main(String[] args) {
         String input = JOptionPane.showInputDialog("Введите выражение");
         String[] lexems = input.split("[ ]");
-        Queue q = new Queue();
         int mult = 1;
         String toException = "";
+        int result = 0;
+        
         try {
             for (String str : lexems) {
                 toException = str;
@@ -33,7 +34,7 @@ public class Calculator {
                 } else {
                     int k = Integer.parseInt(str);
                     if ((mult != 0) && (k >= 0) && (str.charAt(0) != '+')) {
-                        q.add(mult * Integer.parseInt(str));
+                        result += mult * Integer.parseInt(str);
                         mult = 0;
                     } else {
                         throw new Exception();
@@ -44,46 +45,8 @@ public class Calculator {
             JOptionPane.showMessageDialog(null, input + "= '" + toException + "' " 
                     + ((mult == 0) ? "- Ожидался оператор +/-" : "- Ожидалось число"));
             return;
-            //System.out.println(toException);
         }
-        
-        int result = 0;
-        while (q.hasNext()) {
-            result += q.getNext();
-        }
+
         JOptionPane.showMessageDialog(null, input + " = " + result); 
     }
-}
-
-
-class Queue{
-    
-    private Queue head;
-    private Queue tail;
-    private Queue next;
-    private int element;
-    
-    public Queue() {
-        this.next = null;
-        this.head = this;
-        this.tail = this;
-    }
-    
-    public void add(int a) {
-        Queue buf = new Queue();
-        buf.element = a;
-        this.tail.next = buf;
-        this.tail = buf;
-        
-    }
-    
-    public int getNext(){
-        this.head = this.head.next;
-        return this.head.element;
-    }
-    
-    public boolean hasNext(){
-        return (this.head.next != null);
-    }
-    
 }
